28c40d87b4b2be2e60f38769fbe52e3f848e5270
Jason <lookwhatjasondid@gmail.com>
2019-09-21 07:37:54 -0500

    revise script to accept target IP and target port number

--- grab_banner.py
+++ grab_banner.py
@@ -4,7 +4,18 @@ import socket
 
 socket.setdefaulttimeout(2)
 s = socket.socket()
-s.connect(('www.google.com', 80))
-s.send(b'GET /\n\n')
-banner = s.recv(1024)
-print(banner)
+
+target_ip = input('Enter IP address: ')
+target_port = int(input('Enter port: '))
+
+try:
+    # Connect to the target IP address
+    # s.connect(('www.google.com', 80))
+    s.connect((target_ip, target_port))
+    s.send(b'GET /\n\n')
+    # Return the service being run on the target server
+    banner = s.recv(1024)
+    print(banner)
+    print(type(banner))
+except:
+    print('Unable to connect to %s:%s' % (target_ip, target_port))
