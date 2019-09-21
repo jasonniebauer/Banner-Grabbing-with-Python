b5f3778c0fa4dde8b6715d5013dbbad856d878a1
Jason <lookwhatjasondid@gmail.com>
2019-09-21 07:49:23 -0500

    clean up

--- grab_banner.py
+++ grab_banner.py
@@ -5,17 +5,18 @@ import socket
 socket.setdefaulttimeout(2)
 s = socket.socket()
 
-target_ip = input('Enter IP address: ')
+target_ip = input('Enter address: ')
 target_port = int(input('Enter port: '))
 
 try:
     # Connect to the target IP address
-    # s.connect(('www.google.com', 80))
+    # i.e. ('www.google.com', 80)
     s.connect((target_ip, target_port))
     s.send(b'GET /\n\n')
     # Return the service being run on the target server
     banner = s.recv(1024)
+    # Decode response from bytes to string
+    banner = banner.decode('utf-8')
     print(banner)
-    print(type(banner))
 except:
     print('Unable to connect to %s:%s' % (target_ip, target_port))
